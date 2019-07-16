9681fb46ed3ee66228a4afe340b848bf1f5f8648
Sherman <uxdailygeek@gmail.com>
2019-07-16 23:31:23 +0800

    Tried compiling the app on Linux

--- FastLED_Basics/src/main.cpp
+++ FastLED_Basics/src/main.cpp
@@ -8,6 +8,7 @@
 CRGB leds[NUM_LEDS];
 
 void setup() {
+	Serial.begin(9600);
 	FastLED.addLeds<WS2812, DATA_PIN>(leds, NUM_LEDS);
 }
 
@@ -18,10 +19,13 @@ void loop() {
 
 	int val = analogRead(2);
 
-	int numLedsToLight = map(val, 0, 1023, 0, NUM_LEDS);
+	Serial.println(val);
+
+	int numLedsToLight = map(val, 0, 990, 0, NUM_LEDS);
 
 	// First, clear the existing led values
 	FastLED.clear();
+
 	for (int led = 0; led < numLedsToLight; led++) {
 		leds[led] = CRGB::Blue;
 	}
